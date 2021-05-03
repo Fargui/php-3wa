8062084f0717d9deb29d1b937b473ed013e08814
Rémy 3wa <remy.turpault@3wa.io>
2021-05-03 12:40:37 +0200

    exemples 3 et 4

---
+++ syntaxes-php7/03-interpolation.php
@@ -0,0 +1,6 @@
+<?php
+
+$teacher = "Rémy";
+
+echo "Hello $nom"; // entre guillemets doubles pas besoin de concaténation pour les variables simples
+// (attention: ne marche pas pour éléments de tableau, pour propriétés d'objet... )
