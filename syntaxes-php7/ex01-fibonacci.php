8f4b9cd6731593b4e7bae3b51d92ae9baf30a970
Rémy 3wa <remy.turpault@3wa.io>
2021-05-03 12:36:36 +0200

    + en commentaire retour valeur finale

--- syntaxes-php7/ex01-fibonacci.php
+++ syntaxes-php7/ex01-fibonacci.php
@@ -9,12 +9,17 @@ function fib(int $nb = 30): Generator {
     yield $curr;
     [$prev, $curr] = [$curr, $prev+$curr];
   }
+  //return $curr;
 }
 
 foreach(fib() as $v) {
   echo $v.'<br>';
 }
 
+// foreach($gen = fib() as $v) {
+//   echo $v.'<br>';
+// }
+// echo 'Chiffre n°'.($v+1).' de la suite de fibonacci : '.$gen->getReturn();
 
 // exo: écrire une fonction générateur yieldant les 30 premiers nombres de la suite de fibonacci
 // 1 1 2 3 5 8 13 21....
