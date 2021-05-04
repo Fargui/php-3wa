d0f1ae3e87cdf5cd2cc7896ad63784ed61bd1e44
Rémy 3wa <remy.turpault@3wa.io>
2021-05-04 14:43:26 +0200

    base parking

---
+++ 02-relations-classes/ex5-synthese/src/Parking.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Park;
+
+use Park\Road\Parkable;
+
+class Parking {
+  private array $parkables = [];
+
+  public function addPark(Parkable $parkable) {
+
+  }
+  public function removePark(Parkable $parkable) {
+
+  }
+  public function count(): integer {
+    
+  }
+}
