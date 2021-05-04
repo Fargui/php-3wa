86ff0fc3df34e1784b0b530d47cded72878086c6
Fargui <gui.dumez@gmail.com>
2021-05-04 08:43:37 +0200

    -

---
+++ 02-relations-classes/ex2-news-v3/vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '0c985564a2602fb7a0f759d09d29e8f22d495189',
+    'name' => 'guillaumedumez/ex2-news-v3',
+  ),
+  'versions' => 
+  array (
+    'guillaumedumez/ex2-news-v3' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '0c985564a2602fb7a0f759d09d29e8f22d495189',
+    ),
+  ),
+);
